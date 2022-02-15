9f9ae678fab73d9a88bb06309db140534ea5b658
Rosa Regalado <rosaregalado101@gmail.com>
2022-02-15 15:59:57 -0800

    dynamic content and footer

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -3,16 +3,17 @@ import './POPOSSpace.css';
 
 function POPOSSpace(props) {
   // create props object
-  const { name, image, address} = props
+  const { name, image, address, hours} = props
   return (
     <div>
+      <h1>{name}</h1>
       <img src={`${process.env.PUBLIC_URL}/images/${image}`}     
         width="300"
         height="300"
         alt="hello"
       />
-      <h1>{name}</h1>
       <div>{address}</div>
+      <div className="hours">{hours}</div>
     </div>
   )
 }
