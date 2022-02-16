7cb9ecf809f854858a22b4ca97cb279fbdf01277
Rosa Regalado <rosaregalado101@gmail.com>
2022-02-15 23:24:36 -0800

    displayed feature emojis

--- src/POPOSDetails.js
+++ src/POPOSDetails.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router'
 import data from './sfpopos-data.js'
+import POPOSFeatureList from './POPOSFeatureList'
 
 function POPOSDetails(props) {
   const params = useParams()
@@ -16,7 +17,7 @@ function POPOSDetails(props) {
         <h1 className="POPOSDetails-title">{ title }</h1>
         <p className="POPOSDetails-desc">{ desc }</p>
         <p className="POPOSDetails-hours">{ hours }</p>
-        <p className="POPOSDetails-features">{ features }</p>
+        <POPOSFeatureList features={features}/>
         <p className="POPOSDetails-geo">{ geo.lat } { geo.lon }</p>
       </div>
     </div>
